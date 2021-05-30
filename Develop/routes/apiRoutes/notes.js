8798ddbe9d4cefbeb1ba4fc336103c3869b95423
CBear <chebert222@gmail.com>
2021-05-30 15:41:54 -0700

    note route delete done

--- Develop/routes/apiRoutes/notes.js
+++ Develop/routes/apiRoutes/notes.js
@@ -1,21 +1,37 @@
 const router = require('express').Router();
 const fs = require('fs');
-const notes = require('../../db/db.json')
+let notes = require('../../db/db.json')
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
-  res.json(notes)
-  fs.readFile.json(notes);
+notes = require('../../db/db.json');
+  res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  const pushNote = req.body;
+  pushNote.id = uuidv4();
+  notes.push(pushNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+    if (err) throw err
+
   });
-  
-  router.post('/notes', (req, res) => {
-    const newNotes = req.body;
-  
-    
-    notes.push(newNotes);
-    
-    fs.writeFile.json(newNotes);
-    res.json(newNotes);
+
+  res.json(notes);
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const keptNotes = notes.filter((note) => {
+    return note.id !== id;
   })
-  
+  fs.writeFile('./db/db.json', JSON.stringify(keptNotes), (err, data) => {
+    if (err) throw err
+    res.json(keptNotes);
+  });
+
+});
+
 
 module.exports = router;
